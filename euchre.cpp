ee3f67d79e5e79d58a9997a10b91510ee586f791
vishal-myneni <158111187+vishal-myneni@users.noreply.github.com>
2026-02-24 21:59:42 -0500

    Update euchre.cpp

--- euchre.cpp
+++ euchre.cpp
@@ -51,10 +51,75 @@ class Game {
           
         Card upcard = pack.deal_one();
         return upcard;
-}
-      void make_trump(/* ... */);
-      void play_hand(/* ... */);
-      // ...
+    }
+      Suit make_trump(const Card &upcard, int dealer, int &maker_idx) {
+        Suit order_up_suit;
+
+        for (int i = 1; i <= 4; ++i) {
+            int player_idx = (dealer + i) % 4;
+            bool is_dealer = (player_idx == dealer);
+    
+            if (players[player_idx]->make_trump(upcard, is_dealer, 1, order_up_suit)) {
+                cout << *players[player_idx] << " orders up " << order_up_suit << endl;
+                players[dealer]->add_and_discard(upcard);
+                cout << endl;
+                maker_idx = player_idx;
+                return order_up_suit;
+            } else {
+                cout << *players[player_idx] << " passes" << endl;
+            }
+        }
+    
+        for (int i = 1; i <= 4; ++i) {
+            int player_idx = (dealer + i) % 4;
+            bool is_dealer = (player_idx == dealer);
+    
+            if (players[player_idx]->make_trump(upcard, is_dealer, 2, order_up_suit)) {
+                cout << *players[player_idx] << " orders up " << order_up_suit << endl;
+                cout << endl;
+                maker_idx = player_idx;
+                return order_up_suit;
+            } else {
+                cout << *players[player_idx] << " passes" << endl;
+            }
+        }
+    }
+      void play_tricks(int dealer, Suit trump, int maker_idx,
+                 int &tricks02, int &tricks13) {
+    
+        int leader = (dealer + 1) % 4;
+    
+        for (int trick = 0; trick < 5; ++trick) {
+    
+            Card led_card = players[leader]->lead_card(trump);
+            cout << led_card << " led by " << *players[leader] << endl;
+    
+            Card winning_card = led_card;
+            int winning_player = leader;
+    
+            for (int i = 1; i < 4; ++i) {
+                int player_idx = (leader + i) % 4;
+                Card played = players[player_idx]->play_card(led_card, trump);
+                cout << played << " played by " << *players[player_idx] << endl;
+    
+                if (Card_less(winning_card, played, led_card, trump)) {
+                    winning_card = played;
+                    winning_player = player_idx;
+                }
+            }
+    
+            cout << *players[winning_player] << " takes the trick" << endl;
+            cout << endl;
+    
+            if (winning_player == 0 || winning_player == 2) {
+                tricks02++;
+            } else {
+                tricks13++;
+            }
+    
+            leader = winning_player;
+        }
+    }
 };
 
 int main(int argc, char *argv[]) {
